1ab6b58af12abbe1ef1138e1ae0902776bfbf87f
JonathanEsteves <jonathan.dev_93@outlook.com>
2025-03-04 23:26:43 -0300

    feat: slug migrate field db and router to restautantpage

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,13 @@
+interface RestaurantPageProps {
+   params: Promise<{slug:string}>
+}
+
+const RestaurantPage = async ({params}: RestaurantPageProps) => {
+   const {slug} = await params
+   return (
+      <h1>{slug}</h1>
+   );
+
+}
+
+export default RestaurantPage
