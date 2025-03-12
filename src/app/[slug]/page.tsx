5a5648219cf05a8f4c98b211d9e74b4b8e0b805b
JonathanEsteves <jonathan.dev_93@outlook.com>
2025-03-11 23:11:40 -0300

    chore: news settings in tsconfig nextconfig schemaprisma

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,20 +1,24 @@
+//import { ConsumptionMethod } from "@prisma/client";
 import Image from "next/image";
 import { notFound } from "next/navigation";
 
 import { db } from "@/lib/prisma";
-//import { ConsumptionMethod } from "@prisma/client";
+
+import ConsumptionMethodOption from "./components/consumption-method-option";
+
 
 interface RestaurantPageProps {
-   params: Promise<{slug:string}>
+   params: Promise<{slug: string}>;
 }
 
 const RestaurantPage = async ({params}: RestaurantPageProps) => {
    const {slug} = await params;
-   const restaurant = await db.restaurant.findUnique({where: {slug}}); // faz uma solicitação direto ao db para encontrar o slug @unique
+   const restaurant = await db.restaurant.findUnique({where:{slug} }); // faz uma solicitação direto ao db para encontrar o slug @unique
    if (!restaurant) {
       return notFound(); // caso não seja encontrado o slug, irá retornar que não foi encontrado este campo no db
    }
    return (
+   <div className="container mx-auto h-full w-[425px]">
      <div className ="flex h-screen flex-col items-center justify-center px-6 pt-24" >
       {/* Logo e Título */}
       <div className= "flex flex-col items-center gap-2">
@@ -25,25 +29,36 @@ const RestaurantPage = async ({params}: RestaurantPageProps) => {
             height={82}
          />
          <h2 className="font-semibold">{restaurant.name}</h2>
-      </div>
-      {/* Seja bem vindo! */ }
-      <div className="space-y-2 pt-24 text-center">
-         <h3 className="text-2xl font-semibold "> Seja muito bem-vindo!</h3>
+       </div>
+       {/*SEJA BEM VINDO!*/}
+       <div className="space-y-2 pt-24 text-center">
+         <h3 className="text-2xl font-semibold">Seja bem vindo!</h3>
          <p className="opacity-55">
-            Escolha como prefere saborear sua refeição. Estamos á sua disposição para oferecer uma experiência única em cada detalhe!
+            Escolha como prefere saborear sua refeição.
+            Estamos á disposição para lhe oferecer uma experiência única em cada detalhe!
          </p>
-      </div>
-      <div className="grid grid-cols-1 gap-4 pt-14">
-          {/*<ConsumptionMethod
-         slug={slug}
+     </div>
+     <div className="grid grid-cols-2 gap-4 pt-14">
+      <ConsumptionMethodOption
          option="DINE_IN"
+         slug={slug}
          buttonText="Para comer aqui"
          imageAlt="Comer aqui"
-         />  */}
-      </div>
+         imageUrl="/dine_in.png"
+         />
+         <ConsumptionMethodOption
+            option="TAKEAWAY"
+            slug={slug}
+            buttonText="Para levar"
+            imageAlt="Para levar"
+            imageUrl="/takeaway.png"
+         />
+         </div>
      </div>
+   </div>
    );
 };
+
 
 // este component é um server components = renderizam no servidor da aplicação
 // podem ser async
@@ -51,4 +66,4 @@ const RestaurantPage = async ({params}: RestaurantPageProps) => {
 // não podem usar eventos ex: onClick
 // não podem utilizar hooks ex: useState, useEffect
 
-export default RestaurantPage
+export default RestaurantPage;
--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -5,7 +5,7 @@ const ProductPage = () => {
   return (
     <div className="p-5 border border-blue-500 rounded-xl">
       <h1 className="text-white bg-gray-500"> Hello Next!</h1>
-      <Button>FSW DONALDS@John</Button>
+      <Button>GM DONALDS@John</Button>
       <Input placeholder="Let's go finish this project!" />
     </div>
   );
